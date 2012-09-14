d6bc6bde3ef386b9226a60eef5946eb4320bb371
Osman <lima.osman@gmail.com>
2012-09-14 09:12:15 -0700

    HelloWorld IOS

---
+++ Hello-World/Hello-World/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  Hello-World
+//
+//  Created by oz on 14/09/12.
+//  Copyright (c) 2012 oz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
---
+++ Hello-World/Hello-World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello-World
+//
+//  Created by oz on 14/09/12.
+//  Copyright (c) 2012 oz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloWorldAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
+    }
+}
